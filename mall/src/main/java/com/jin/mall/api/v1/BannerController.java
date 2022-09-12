7946a1f44af2f0a4ccd2af116aa663fcb55e1ce0
JOYBOY <1781073841@qq.com>
2022-09-12 20:01:17 +0800

    z

--- mall/src/main/java/com/jin/mall/api/v1/BannerController.java
+++ mall/src/main/java/com/jin/mall/api/v1/BannerController.java
@@ -1,5 +1,17 @@
 package com.jin.mall.api.v1;
 
-public class BannerController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class BannerController {
+    @GetMapping("/test")
+    public String test(HttpServletResponse response){
+        return "你好";
+    }
 }
