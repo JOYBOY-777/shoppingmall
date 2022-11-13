48f159af4df729b3f62e2fb687c0d41fb7074e03
JOYBOY <1781073841@qq.com>
2022-11-13 20:18:22 +0800

    z

--- lol/src/main/java/Test/Main.java
+++ lol/src/main/java/Test/Main.java
@@ -1,8 +1,22 @@
 package Test;
 
+import awcward.hero.Diana;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        A a = new A();
-        System.out.println(a.print());
+        String name = Main.getHeroName();
+        if (name.equals("Diana")){
+            Diana diana = new Diana();
+            diana.q();
+        }
+    }
+
+    private static String getHeroName(){
+        System.out.println("请输入一个英雄的名字");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        return name;
     }
 }
