29611d56948f59f426afa9c915130cb0c79ce1c7
JOYBOY <1781073841@qq.com>
2022-11-14 17:22:46 +0800

    第二版本代码switch case

--- lol/src/main/java/Test/Main.java
+++ lol/src/main/java/Test/Main.java
@@ -1,22 +1,30 @@
 package Test;
 
+import awcward.hero.Camille;
 import awcward.hero.Diana;
+import awcward.hero.Irelia;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String name = Main.getHeroName();
-        if (name.equals("Diana")){
-            Diana diana = new Diana();
-            diana.q();
+        switch (name){
+            case "Diana":
+                Diana diana = new Diana();
+                diana.r();
+            case "Irelia":
+                Irelia irelia = new Irelia();
+                irelia.r();
+            case "Camille":
+                Camille camille = new Camille();
+                camille.r();
         }
     }
 
     private static String getHeroName(){
         System.out.println("请输入一个英雄的名字");
         Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-        return name;
+        return in.nextLine();
     }
 }
